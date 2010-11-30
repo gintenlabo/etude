178e6c0716943115e2ed2d46c1c6d04af8500882
SubaruG <gintensubaru@gmail.com>
2010-12-01 04:44:12 +0900

    apply_in_place のコメントを追記

--- etude/memory/apply_in_place.hpp
+++ etude/memory/apply_in_place.hpp
@@ -1,6 +1,6 @@
 //
 //  apply_in_place:
-//    Boost.InPlaceFactory の C++0x 拡張
+//    Boost.InPlaceFactory の適用関数
 // 
 //    InPlaceFactory の apply を自由関数で行えるようにしたものです。
 //    これにより、 move 出来る場合は保持している引数を move するようになるので、
@@ -8,7 +8,7 @@
 //    また、 InPlaceFactory と TypedInPlaceFactory で微妙に表記が異なるのを避けるため、
 //    TypedInPlaceFactory でも、関連付けられた型が一致するなら、
 //    InPlaceFactory と同様のインターフェイスで扱えるように工夫されています。
-//    また、 apply_in_place が呼び出せるかどうかを問い合わせるメタ関数も用意しました。
+//    その際、 apply_in_place が呼び出せるかどうかを問い合わせるメタ関数も実装。
 //    
 //  Copyright (C) 2010  Takaya Saito (SubaruG)
 //    Distributed under the Boost Software License, Version 1.0.
@@ -50,7 +50,7 @@ namespace etude {
   template<class InPlace, class T, class = void>
   struct is_in_place_applyable_
     : is_in_place_factory<InPlace> {};
-  
+  // associated type が定義されない場合は SFINAE で上の定義が使われる
   template<class InPlace, class T>
   struct is_in_place_applyable_< InPlace, T,
     typename std::enable_if<
@@ -59,25 +59,24 @@ namespace etude {
       >::value
     >::type
   > : std::true_type {};
-  
+  // 実装用メタ関数とインターフェイスは分けるのが僕の流儀
   template<class InPlace, class T>
   struct is_in_place_applyable : is_in_place_applyable_<InPlace, T> {};
   
-  
-  // dispatch
+  // 基底クラスによる dispatch
   // Boost.InPlaceFactory 用
   template<class T, class InPlace>
   inline T* apply_in_place_( in_place_factory_base const*, InPlace && x, void* addr ) {
     std::forward<InPlace>(x).template apply<T>( addr );
     return static_cast<T*>( addr );
   }
-  // TypedInPlaceFactory 用
+  // TypedInPlaceFactory 用。直接呼ばれることを想定していないので型チェックは無し
   template<class T, class InPlace>
   inline T* apply_in_place_( typed_in_place_factory_base const*, InPlace && x, void* addr ) {
     return apply_typed_in_place( std::forward<InPlace>(x), addr );
   }
   
-  // インターフェイス
+  // apply_in_place 本体
   template<class T, class InPlace,
     class = typename std::enable_if<is_in_place_applyable<InPlace, T>::value>::type>
   inline T* apply_in_place( InPlace && x, void* addr )
