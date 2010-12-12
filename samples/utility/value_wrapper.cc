877e89b233988360034aef5244ad37ad9089a827
SubaruG <gintensubaru@gmail.com>
2010-12-12 22:31:27 +0900

    value_wrapper のテストを追加

---
+++ samples/utility/value_wrapper.cc
@@ -0,0 +1,65 @@
+//
+//  value_wrapper.hpp の 実用例
+// 
+//    要するにこんなことが出来ます。
+//    
+//  Copyright (C) 2010  Takaya Saito (SubaruG)
+//    Distributed under the Boost Software License, Version 1.0.
+//    http://www.boost.org/LICENSE_1_0.txt
+//
+#include "../../etude/utility/value_wrapper.hpp"
+#include "../../etude/noncopyable.hpp"
+
+#include <tuple>
+#include <utility>
+
+// 任意のクラスに対し、引数を pack したタプルから one-phase construction できる。
+// やってみよう。
+#include <iostream>
+
+struct person
+  : etude::noncopyable
+{
+  std::string name;
+  int age;
+  
+  person( std::string const& name_, int age_ )
+    : name(name_), age(age_) {}
+    
+  person( std::string && name_, int age_ )
+    : name( std::move(name_) ), age(age_) {}
+
+  void foo()
+  {
+    std::cout << name << " (" << age << "): Foo!\n";
+  }
+  
+};
+
+
+int main()
+{
+  // 普通に構築
+  etude::value_wrapper<person> a_( etude::emplace_construct, "A", 19 );
+  auto& a = a_.get();
+  a.foo();
+  
+  // タプルから one-phase construction
+  etude::value_wrapper<person> b_( etude::unpack_construct, std::make_tuple( "B", 21 ) );
+  auto& b = b_.get();
+  b.foo();
+  
+  // タプル（ lvalue ）でも問題ないよ？
+  // もっと言うと、 pair でもいいんだよ？
+  std::pair<char const*, int> p( "C", 18 );
+  etude::value_wrapper<person> c_( etude::unpack_construct, p );
+  auto& c = get(c_);
+  c.foo();
+  
+  // 毎回 get するの面倒？ うん、僕もそう思う。
+  // でも、このクラスを直接使うことは僕としては考えてなくて、
+  // 今回みたいに & で受けるか、あるいはもっと多い例としては、
+  // クラスのメンバとして使うことを想定して書いたんだ。
+  // なので、 value_wrapper を使うクラスで、その都度アクセサを書いて欲しい。
+  // EBO に対応してる関係上、メンバ変数として公開することも出来ないしね。
+}
