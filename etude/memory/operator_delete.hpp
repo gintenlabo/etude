32653d669d04d7d36021fe5e450e8652918d19f5
SubaruG <gintensubaru@gmail.com>
2010-12-15 14:09:37 +0900

    適切な operator delete を呼ぶ関数を追加

---
+++ etude/memory/operator_delete.hpp
@@ -0,0 +1,67 @@
+//
+//  operator_delete:
+//    operator delete を呼ぶ関数
+// 
+//    operator_delete<T>(p) は、
+//    T::operator delete があるなら T::operator delete(p) を、
+//    ないならば ::operator delete(p) を呼び出します。
+//    
+//  Copyright (C) 2010  Takaya Saito (SubaruG)
+//    Distributed under the Boost Software License, Version 1.0.
+//    http://www.boost.org/LICENSE_1_0.txt
+//
+#ifndef ETUDE_MEMORY_INCLUDED_OPERATOR_DELETE_HPP_
+#define ETUDE_MEMORY_INCLUDED_OPERATOR_DELETE_HPP_
+
+#include <new>
+
+namespace etude {
+
+  // 実装関数
+  
+  // T::operator delete があるならこっち
+  template<class T,
+    class = decltype( T::operator delete( (void*)0 ) )
+  >
+  inline void operator_delete_( void* p, int ) {
+    T::operator delete(p);
+  }
+  
+  // ないならこっち
+  template<class T>
+  inline void operator_delete_( void* p, ... ) {
+    ::operator delete(p);
+  }
+  
+  // 本体
+  template<class T>
+  inline void operator_delete( void* p ) {
+    return operator_delete_<T>( p, 0 );
+  }
+  
+  
+  // 配列版
+  
+  // T::operator delete [] があるならこっち
+  template<class T,
+    class = decltype( T::operator delete[]( (void*)0 ) )
+  >
+  inline void operator_array_delete_( void* p, int ) {
+    T::operator delete [] (p);
+  }
+  
+  // ないならこっち
+  template<class T>
+  inline void operator_array_delete_( void* p, ... ) {
+    ::operator delete [] (p);
+  }
+  
+  // 本体
+  template<class T>
+  inline void operator_array_delete( void* p ) {
+    return operator_array_delete_<T>( p, 0 );
+  }
+  
+} // namespace etude
+
+#endif  // #ifndef ETUDE_MEMORY_INCLUDED_OPERATOR_DELETE_HPP_
---
+++ tests/memory/operator_delete.cc
@@ -0,0 +1,104 @@
+//
+//  etude/memory/operator_delete.hpp に対するテスト
+//
+//  Copyright (C) 2010  Takaya Saito (SubaruG)
+//    Distributed under the Boost Software License, Version 1.0.
+//    http://www.boost.org/LICENSE_1_0.txt
+//
+#include "../../etude/memory/operator_delete.hpp"
+
+#include <boost/assert.hpp>
+
+template<class T>
+void test()
+{
+  T* p = new T();
+  p->~T();
+  etude::operator_delete<T>(p);
+  
+  int const n = 10;
+  
+  p = new T[n];
+  for( int i = n; i --> 0; ) {
+    (p+i)->~T();
+  }
+  etude::operator_array_delete<T>(p);
+}
+
+template<class T>
+void bad_test()
+{
+  T* p = new T();
+  p->~T();
+  ::operator delete(p); // あっ
+  
+  int const n = 10;
+  
+  p = new T[n];
+  for( int i = n; i --> 0; ) {
+    (p+i)->~T();
+  }
+  ::operator delete[](p); // …。
+}
+
+struct hoge
+{
+  static void* operator new( std::size_t size ) {
+    void* const p = ::operator new( size );
+    instances_() += 1;
+    return p;
+  }
+  static void operator delete( void* p ) {
+    ::operator delete(p);
+    instances_() -= 1;
+  }
+  
+  static void* operator new []( std::size_t size ) {
+    void* const p = ::operator new[]( size );
+    arrays_() += 1;
+    return p;
+  }
+  static void operator delete[]( void* p ) {
+    ::operator delete[](p);
+    arrays_() -= 1;
+  }
+  
+  static int count_instances() {
+    return instances_();
+  }
+  static int count_arrays() {
+    return arrays_();
+  }
+  
+  int x;
+  
+ private:
+  static int& instances_() {
+    static int instances = 0;
+    return instances;
+  }
+  static int& arrays_() {
+    static int arrays = 0;
+    return arrays;
+  }
+  
+};
+
+int main()
+{
+  // 普通の型に対して
+  test<int>();
+  
+  // operator delete がある型について
+  test<hoge>();
+  
+  BOOST_ASSERT( hoge::count_instances() == 0 );
+  BOOST_ASSERT( hoge::count_arrays() == 0 );
+  
+  // 一応、 operator_delete じゃなければダメな理由を。
+  bad_test<hoge>();
+  // ::operator delete の場合、確かに T::operator delete は呼ばれない。
+  BOOST_ASSERT( hoge::count_instances() == 1 );
+  BOOST_ASSERT( hoge::count_arrays() == 1 );
+  
+}
