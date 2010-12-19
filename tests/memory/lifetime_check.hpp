29e1b9564b28209febecb124e432809f50317d5e
SubaruG <gintensubaru@gmail.com>
2010-12-19 18:21:18 +0900

    テスト用ユーティリティlifetime_checkを別ヘッダに

---
+++ tests/memory/lifetime_check.hpp
@@ -0,0 +1,60 @@
+//
+//  lifetime_check: 解放漏れやらなんやらをチェックするクラス
+//    テスト用ユーティリティの一つです。
+//
+//  Copyright (C) 2010  Takaya Saito (SubaruG)
+//    Distributed under the Boost Software License, Version 1.0.
+//    http://www.boost.org/LICENSE_1_0.txt
+//
+#ifndef ETUDE_TEST_MEMORY_INCLUDED_LIFETIME_CHECK_HPP_
+#define ETUDE_TEST_MEMORY_INCLUDED_LIFETIME_CHECK_HPP_
+
+#include <boost/assert.hpp>
+#include <set>
+
+// 寿命チェック用オブジェクト
+template<class Derived>
+struct lifetime_check
+{
+  static bool is_initialized( void const* p ) {
+    if( p == 0 ) {
+      return false;
+    }
+    return get_object_list_().find(p) != get_object_list_().end();
+  }
+  
+  static bool count_existing_instances() {
+    return get_object_list_().size();
+  }
+  
+ protected:
+  lifetime_check() {
+    register_instance_( static_cast<Derived*>(this) );
+  }
+  lifetime_check( lifetime_check const& ) {
+    register_instance_( static_cast<Derived*>(this) );
+  }
+  ~lifetime_check() {
+    unregister_instance_( static_cast<Derived*>(this) );
+  }
+  
+ private:
+  typedef std::set<void const*> object_list_t;
+  
+  static object_list_t& get_object_list_() {
+    static object_list_t object_list;
+    return object_list;
+  }
+  
+  static void register_instance_( void const* p ){
+    bool const succeeded = get_object_list_().insert(p).second;
+    BOOST_ASSERT( succeeded );
+  }
+  static void unregister_instance_( void const* p ){
+    std::size_t erased = get_object_list_().erase(p);
+    BOOST_ASSERT( erased == 1 );
+  }
+  
+};
+
+#endif  // #ifndef ETUDE_TEST_MEMORY_INCLUDED_LIFETIME_CHECK_HPP_
--- tests/memory/test_utilities.hpp
+++ tests/memory/test_utilities.hpp
@@ -10,9 +10,8 @@
 #ifndef ETUDE_TEST_MEMORY_INCLUDED_TEST_UTILITIES_HPP_
 #define ETUDE_TEST_MEMORY_INCLUDED_TEST_UTILITIES_HPP_
 
+#include <utility>
 #include <type_traits>
-#include <boost/assert.hpp>
-#include <set>
 
 // T 型を格納できるストレージ
 #include "../../etude/memory/storage.hpp"
@@ -22,46 +21,6 @@ using etude::storage_of;
 #include "../../etude/memory/pseudo_destructor_call.hpp"
 using etude::pseudo_destructor_call;
 
-// 寿命チェック用オブジェクト
-template<class Tag>
-struct lifetime_check
-{
-  lifetime_check() {
-    register_instance(this);
-  }
-  lifetime_check( lifetime_check const& ) {
-    register_instance(this);
-  }
-  ~lifetime_check() {
-    unregister_instance(this);
-  }
-  
-  static bool is_initialized( void const* p ) {
-    return get_object_list().find(p) != get_object_list().end();
-  }
-  
-  static bool count_existing_instances() {
-    return get_object_list().size();
-  }
-  
- private:
-  typedef std::set<void const*> object_list_t;
-  
-  static object_list_t& get_object_list() {
-    static object_list_t object_list;
-    return object_list;
-  }
-  
-  static void register_instance( void const* p ){
-    bool const succeeded = get_object_list().insert(p).second;
-    BOOST_ASSERT( succeeded );
-  }
-  static void unregister_instance( void const* p ){
-    std::size_t erased = get_object_list().erase(p);
-    BOOST_ASSERT( erased == 1 );
-  }
-  
-};
-
+#include "lifetime_check.hpp"
 
 #endif  // #ifndef ETUDE_TEST_MEMORY_INCLUDED_TEST_UTILITIES_HPP_
