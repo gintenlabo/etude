1b38c403ee8134a199ac2221f346cbd6258b4b1a
SubaruG <gintensubaru@gmail.com>
2011-03-08 02:33:40 +0900

    tuple_init を追加

--- tests/utility/tuple_init.cc
+++ tests/utility/tuple_init.cc
@@ -1,14 +1,15 @@
 //
-//  tuple_tail のテストです。
+//  tuple_init のテストです。
 //    
 //  Copyright (C) 2011  Takaya Saito (SubaruG)
 //    Distributed under the Boost Software License, Version 1.0.
 //    http://www.boost.org/LICENSE_1_0.txt
 //
 
-#include "../../etude/utility/tuple_tail.hpp"
+#include "../../etude/utility/tuple_init.hpp"
 
 #define STATIC_ASSERT( expr ) static_assert( expr, #expr )
+
 #include <boost/test/minimal.hpp>
 
 #include <type_traits>
@@ -24,7 +25,7 @@ int test_main( int, char** )
   {
     int i = 0;
     std::tuple<int, int&, int const&, int&&> p( 23, i, i, std::move(i) );
-    typedef std::tuple<int&, int const&, int&&> tuple_type;
+    typedef std::tuple<int, int&, int const&> tuple_type;
     
     STATIC_ASSERT((
       std::is_same< decltype( etude::tuple_tail(p) ), tuple_type >::value
@@ -35,15 +36,14 @@ int test_main( int, char** )
       std::is_same< decltype(t), tuple_type >::value
     ));
     
-    BOOST_CHECK( &std::get<0>(t) == &i );
+    BOOST_CHECK( std::get<0>(t) == 23 );
     BOOST_CHECK( &std::get<1>(t) == &i );
     BOOST_CHECK( &std::get<2>(t) == &i );
   }
   
   {
-    char const* const s = "hoge";
-    std::pair<int, char const*> p( 23, s );
-    typedef std::tuple<char const*> tuple_type;
+    std::pair<int, char const*> p( 23, "hoge" );
+    typedef std::tuple<int> tuple_type;
     
     STATIC_ASSERT((
       std::is_same< decltype( etude::tuple_tail(p) ), tuple_type >::value
@@ -54,7 +54,7 @@ int test_main( int, char** )
       std::is_same< decltype(t), tuple_type >::value
     ));
     
-    BOOST_CHECK( std::get<0>(t) == s );
+    BOOST_CHECK( std::get<0>(t) == 23 );
   }
   
   {
@@ -70,8 +70,8 @@ int test_main( int, char** )
       std::is_same< decltype(t), tuple_type >::value
     ));
     
-    BOOST_CHECK( std::get<0>(t) == 2 );
-    BOOST_CHECK( std::get<1>(t) == 3 );
+    BOOST_CHECK( std::get<0>(t) == 1 );
+    BOOST_CHECK( std::get<1>(t) == 2 );
   }
   
   return 0;
