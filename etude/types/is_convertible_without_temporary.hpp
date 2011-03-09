6dd3c5825a19bdec7e93e31e5f418b73342e3d71
SubaruG <gintensubaru@gmail.com>
2011-03-10 05:16:59 +0900

    is_convertible_without_temporary の説明を追加

--- etude/types/is_convertible_without_temporary.hpp
+++ etude/types/is_convertible_without_temporary.hpp
@@ -19,6 +19,28 @@
 //    [ note:
 //      これにより、一時オブジェクトを介する型変換を抑止しつつ、
 //      それ以外の型変換は可能なかぎり許した型変換可能性を調査することができます。
+//    
+//      具体的には、etude::is_convertible_without_temporary<From, To> は、
+//        To test( From && x ) {
+//          return std::forward<From>(x);
+//        }
+//      という関数 test が有ったとき、 return 文が well-formed で、かつ
+//      関数 test が return 文中で生成された一時オブジェクトへのアドレスを返さない場合
+//      には std::true_type を、それ以外の場合には std::false_type を継承します。
+//    
+//      ただし、現行の実装は、少々問題点があり、
+//        template<class T>
+//        struct X {
+//          T x;
+//          operator T const& () const& { return x; }
+//          operator T && () && { return std::forward<T>(x); }
+//        };
+//      というクラス X が有った場合、
+//      is_convertible_without_temporary<X<T>&&, T&&>::value は true になるべきですが、
+//      現行の実装では false になってしまいます。
+//    
+//      そこを何とかしたいとは思っているのですが、うまい実装を思いつかないのが現状です。
+//    
 //    ]
 //
 //  Copyright (C) 2011  Takaya Saito (SubaruG)
