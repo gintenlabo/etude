8fbaf0e064e7a50f149356e1190cd3b9db917566
SubaruG <gintensubaru@gmail.com>
2011-03-13 18:50:24 +0900

    optional の実装クラスを別ヘッダに分離

--- etude/optional.hpp
+++ etude/optional.hpp
@@ -14,259 +14,30 @@
 #ifndef ETUDE_INCLUDED_OPTIONAL_HPP_
 #define ETUDE_INCLUDED_OPTIONAL_HPP_
 
+#include <boost/none.hpp>
+#include "detail_/optional_impl_.hpp"
+
 #include <utility>
 #include <type_traits>
 
-#include <boost/none.hpp>
 #include <boost/assert.hpp>
 #include <boost/utility/addressof.hpp>
-
-#include "types/decay_and_strip.hpp"
-#include "types/is_assignable.hpp"
-#include "types/is_assignable_or_convertible.hpp"
-#include "memory/storage.hpp"
-#include "memory/apply_in_place.hpp"
-#include "utility/compressed_pair.hpp"
-#include "utility/emplace_construct.hpp"
-
-#include "types/is_equality_comparable.hpp"
-#include "types/is_less_than_comparable.hpp"
-#include "types/is_less_or_equal_comparable.hpp"
-#include "types/is_assignable.hpp"
-#include "operators/totally_ordered.hpp"
-#include "operators/partially_ordered.hpp"
 
 #include "utility/empty_base.hpp"
 #include "noncopyable.hpp"
 #include "immovable.hpp"
+#include "operators/totally_ordered.hpp"
+#include "operators/partially_ordered.hpp"
+
+#include "types/is_equality_comparable.hpp"
+#include "types/is_less_than_comparable.hpp"
+#include "types/is_less_or_equal_comparable.hpp"
+#include "types/is_assignable_or_convertible.hpp"
+
+#include "types/decay_and_strip.hpp"
 
 namespace etude {
 
-  template<class T>
-  class optional;
-
-  template<class T>
-  struct optional_impl_base_
-  {
-    // construct
-    optional_impl_base_()
-      : impl_() {}  // impl_ の ctor で first は初期化されて false になる
-    // copy/move construct
-    optional_impl_base_( optional_impl_base_ const& src )
-      : impl_()
-    {
-      if( auto const p = src.get() ) {
-        construct( *p );
-      }
-    }
-    optional_impl_base_( optional_impl_base_ && src )
-      : impl_()
-    {
-      if( auto const p = src.get() ) {
-        construct( std::forward<T>(*p) );
-      }
-    }
-    // copy/move assign
-    optional_impl_base_& operator=( optional_impl_base_ const& src )
-    {
-      if( auto const p = src.get() ) {
-        this->assign( *p );
-      }
-      else {
-        this->dispose();
-      }
-      return *this;
-    }
-    optional_impl_base_& operator=( optional_impl_base_ && src )
-    {
-      if( auto const p = src.get() ) {
-        this->assign( std::forward<T>(*p) );
-      }
-      else {
-        this->dispose();
-      }
-      return *this;
-    }
-    
-    // observers
-    bool is_initialized() const { return impl_.first(); }
-    
-    void*       address()       { return impl_.second().address(); }
-    void const* address() const { return impl_.second().address(); }
-    
-    T* get() {
-      return is_initialized() ? static_cast<T*>( address() ) : 0;
-    }
-    T const* get() const {
-      return is_initialized() ? static_cast<T const*>( address() ) : 0;
-    }
-    
-    // modifiers
-    void dispose()
-    {
-      if( T* p = this->get() ) {
-        p->~T();
-        impl_.first() = false;
-      }
-    }
-    
-    template<class... Args>
-    void construct( Args&&... args )
-    {
-      dispose();
-      
-      ::new( address() ) T( std::forward<Args>(args)... );
-      impl_.first() = true;
-    }
-    
-    template<class InPlace>
-    void in_place_construct( InPlace && in_place )
-    {
-      dispose();
-      
-      apply_in_place<T>( std::forward<InPlace>(in_place), address() );
-      impl_.first() = true;
-    }
-    
-    template<class U>
-    void assign( U && x )
-    {
-      return assign_( std::forward<U>(x), 0 );
-    }
-    
-    // swap
-    void swap( optional_impl_base_& other )
-    {
-      if( auto const p1 = this->get() ) {
-        if( auto const p2 = other.get() ) {
-          using std::swap;
-          swap( *p1, *p2 );
-        }
-        else {
-          other.construct( std::forward<T>(*p2) );
-          this->dispose();
-        }
-      }
-      else {
-        if( auto const p2 = other.get() ) {
-          this->construct( std::forward<T>(*p2) );
-          other.dispose();
-        }
-      }
-    }
-    
-   private:
-    typedef etude::storage<T> storage_type;
-    etude::compressed_pair<bool, storage_type> impl_;
-    
-    // assign の実装
-    // assignable ならば代入を使う
-    template< class U,
-      class = typename std::enable_if<
-        etude::is_assignable<T, U>::value
-      >::type
-    >
-    void assign_( U && x, int )
-    {
-      if( auto const p = this->get() ) {
-        *p = std::forward<U>(x);
-      }
-      else {
-        construct( std::forward<U>(x) );
-      }
-    }
-    // assignable でないなら construct を使う
-    // 自己代入になりうる場合はチェックする
-    template< class U,
-      class = typename std::enable_if<
-        !etude::is_assignable<T, U>::value &&
-        std::is_convertible<typename std::decay<U>::type*, T*>::type
-      >::type
-    >
-    void assign_( U && x, ... )
-    {
-      if( boost::addressof(x) != this->get() ) {
-        construct( std::forward<U>(x) );
-      }
-    }
-    // それ以外
-    template<class U>
-    void assign_( U && x, ... )
-    {
-      construct( std::forward<U>(x) );
-    }
-    
-  };
-
-  template<class T, class = void>
-  struct optional_impl_
-    : optional_impl_base_<T>
-  {
-    typedef optional_impl_base_<T> base_;
-    
-    optional_impl_() = default;
-    ~optional_impl_(){ base_::dispose(); }
-    
-  };
-  
-  template<class T>
-  struct optional_impl_< T,
-    typename std::enable_if<
-      !std::is_reference<T>::value &&
-      std::has_trivial_destructor<T>::value
-    >::type
-  >
-    : optional_impl_base_<T>
-  {
-    typedef optional_impl_base_<T> base_;
-    
-    optional_impl_() = default;
-    // ~optional_impl_(){ base_::dispose(); }
-    
-  };
-  
-  template<class T>
-  struct optional_impl_< T,
-    typename std::enable_if<std::is_reference<T>::value>::type
-  >
-  {
-    typedef typename std::add_pointer<T>::type pointer;
-    
-    optional_impl_()
-      : p_() {}
-    
-    bool is_initialized() const { return p_; }
-    pointer get() const { return p_; }
-    
-    template<class U>
-    void construct( U && x ) {
-      p_ = boost::addressof(x);
-    }
-    
-    template<class InPlace>
-    void in_place_construct( InPlace && in_place ) = delete;
-    
-    template< class U,
-      class = typename std::enable_if<
-        std::is_convertible<U, T>::value
-      >::type
-    >
-    void assign( U && x ) {
-      p_ = boost::addressof(x);
-    }
-    
-    void dispose() {
-      p_ = 0;
-    }
-    
-    void swap( optional_impl_& other ) {
-      std::swap( p_, other.p_ );
-    }
-    
-   private:
-    pointer p_;
-  };
-  
   // メタ関数 optional_base_
   // T の copy/move の有無に合わせて optional の copy/move の有無を変える
   template<class T>
