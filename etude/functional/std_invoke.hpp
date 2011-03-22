0b82c83e34e15888f53ca74732dc7d21be56b344
SubaruG <gintensubaru@gmail.com>
2011-03-22 19:58:05 +0900

    標準ライブラリの INVOKE を行う関数を追加

---
+++ etude/functional/std_invoke.hpp
@@ -0,0 +1,110 @@
+//
+//  std_invoke:
+//    標準ライブラリ <functional> における INVOKE をシミュレートする関数
+// 
+//    etude::std_invoke( f, t1, t2, ..., tN ) は、
+//      INVOKE( f, t1, t2, ..., tN )
+//    を呼び出します。
+//    ここで、 INVOKE は標準ライブラリ <functional> の規格において定義されたものです。
+//    
+//    etude::invoke<R>( f, t1, t2, ..., tN ) は、
+//    etude::invoke( f, t1, t2, ..., tN ) の戻り値を R に暗黙変換して返します。
+//    これは標準ライブラリの <functional> の規格における
+//    INVOKE( f, t1, t2, ..., tN, R ) の動作と同一です。
+//    ただし R が void の場合、戻り値は暗黙変換ではなく明示的に void に変換されます
+//    （つまり、戻り値は単純に無視されます）。
+//    
+//  Copyright (C) 2011  Takaya Saito (SubaruG)
+//    Distributed under the Boost Software License, Version 1.0.
+//    http://www.boost.org/LICENSE_1_0.txt
+//
+#ifndef ETUDE_FUNCTINAL_INCLUDED_STD_INVOKE_HPP_
+#define ETUDE_FUNCTINAL_INCLUDED_STD_INVOKE_HPP_
+
+#include <utility>
+#include <type_traits>
+#include <functional>
+#include <boost/preprocessor/empty.hpp>
+
+#include "wrap_if_mem_fn.hpp"
+#include "../types/identity.hpp"
+
+namespace etude {
+  
+  // 本体。 規格 20.8.2 Requirements [func.require] で定義される INVOKE の処理を行う
+  template<class F, class... Args,
+    class R = decltype(
+      etude::wrap_if_mem_fn( std::declval<F>() )( std::declval<Args>()... )
+    )
+  >
+  inline R std_invoke( F && f, Args&&... args )
+  {
+    return etude::wrap_if_mem_fn( std::forward<F>(f) )( std::forward<Args>(args)... );
+  }
+  // 型指定版。戻り値を R に暗黙変換する。 R が void ならば単純に捨てる
+  template<class R, class F, class... Args,
+    class R_ = decltype(
+      etude::std_invoke( std::declval<F>(), std::declval<Args>()... )
+    ),
+    class = typename std::enable_if<
+      std::is_void<R>::value || std::is_convertible<R_, R>::value
+    >::type
+  >
+  inline R std_invoke( F && f, Args&&... args )
+  {
+    return static_cast<R>(
+      etude::std_invoke( std::forward<F>(f), std::forward<Args>(args)... )
+    );
+  }
+  
+  // 引数が関数ポインタの場合に型推論を補助
+  template<class R, class... Args>
+  inline R std_invoke( R (*f)( Args... ), typename etude::identity<Args>::type... args )
+  {
+    return f( std::forward<Args>(args)... );
+  }
+  // 型指定版
+  template< class R, class R_, class... Args,
+    class = typename std::enable_if<
+      std::is_void<R>::value || std::is_convertible<R_, R>::value
+    >::type
+  >
+  inline R std_invoke( R_ (*f)( Args... ), typename etude::identity<Args>::type... args )
+  {
+    return static_cast<R>( f( std::forward<Args>(args)... ) );
+  }
+  
+  // メンバ関数ポインタの場合に型推論補助
+  #define ETUDE_GEN_( qualifier )                                               \
+    template<class R, class X, class T, class... Args>                          \
+    inline R std_invoke( R ( X::*f )( Args... ) qualifier,                      \
+      T && x, typename etude::identity<Args>::type... args )                    \
+    {                                                                           \
+      return std::mem_fn(f)( std::forward<T>(x), std::forward<Args>(args)... ); \
+    }                                                                           \
+    template< class R, class R_, class X, class T, class... Args,               \
+      class = typename std::enable_if<                                          \
+        std::is_void<R>::value || std::is_convertible<R_, R>::value             \
+      >::type                                                                   \
+    >                                                                           \
+    inline R std_invoke( R_ ( X::*f )( Args... ) qualifier,                     \
+      T && x, typename etude::identity<Args>::type... args )                    \
+    {                                                                           \
+      return static_cast<R>(                                                    \
+        std::mem_fn(f)( std::forward<T>(x), std::forward<Args>(args)... )       \
+      );                                                                        \
+    }                                                                           \
+    /* ETUDE_GEN_( qual ) */
+    
+    ETUDE_GEN_( BOOST_PP_EMPTY() )
+    ETUDE_GEN_( const )
+    ETUDE_GEN_( volatile )
+    ETUDE_GEN_( const volatile )
+    
+    // 本来はここに & や && 系列も用意する
+    
+  #undef ETUDE_GEN_
+  
+} // namespace etude
+
+#endif  // #ifndef ETUDE_FUNCTINAL_INCLUDED_STD_INVOKE_HPP_
---
+++ tests/functional/std_invoke.cc
@@ -0,0 +1,214 @@
+//
+//  std_invoke のテストです。
+//    
+//  Copyright (C) 2011  Takaya Saito (SubaruG)
+//    Distributed under the Boost Software License, Version 1.0.
+//    http://www.boost.org/LICENSE_1_0.txt
+//
+#include "../../etude/functional/std_invoke.hpp"
+
+#include <type_traits>
+#include <boost/test/minimal.hpp>
+#define STATIC_ASSERT( expr ) static_assert( expr, #expr )
+
+struct Base
+{
+  Base() : member() {}
+  explicit Base( int x ) : member(x) {}
+  
+  int member;
+  
+  int foo() { member *= 2; return member; }
+  int bar( int i ) const { return i * 2 + member; }
+  
+};
+
+struct Derived
+  : Base
+{
+  Derived(){}
+  explicit Derived( int x ) : Base(x) {}
+};
+
+// test1
+// INVOKE(f, t1, t2, ..., tN) is (t1.*f)(t2, ..., tN)
+// when f is a pointer to a member function of a class T and
+// t1 is an object of type T or a reference to an object of type T
+// or a reference to an object of a type derived from T
+void test1()
+{
+  auto foo = &Base::foo;
+  auto bar = &Base::bar;
+  
+  // object of type T
+  BOOST_CHECK( etude::std_invoke( bar, Base(), 2 ) == 4 );
+  {
+    auto x = etude::std_invoke<double>( bar, Base(1), 2 );
+    STATIC_ASSERT(( std::is_same<double, decltype(x)>::value ));
+    BOOST_CHECK( x == 5 );
+  }
+  
+  // reference to an object of type T
+  Base base(1);
+  BOOST_CHECK( etude::std_invoke( foo, base ) == 2 );
+  BOOST_CHECK( etude::std_invoke( bar, base, 2 ) == 6 );
+  {
+    etude::std_invoke<void>( foo, base );
+    BOOST_CHECK( base.member == 4 );
+    
+    auto x = etude::std_invoke<double>( bar, base, 0 );
+    STATIC_ASSERT(( std::is_same<double, decltype(x)>::value ));
+    BOOST_CHECK( x == 4 );
+  }
+  
+  // a reference to an object of a type derived from T
+  Derived derived(2);
+  BOOST_CHECK( etude::std_invoke( foo, derived ) == 4 );
+  BOOST_CHECK( etude::std_invoke( bar, derived, 2 ) == 8 );
+  {
+    etude::std_invoke<void>( foo, derived );
+    BOOST_CHECK( derived.member == 8 );
+    
+    auto x = etude::std_invoke<double>( bar, derived, 0 );
+    STATIC_ASSERT(( std::is_same<double, decltype(x)>::value ));
+    BOOST_CHECK( x == 8 );
+  }
+}
+
+// test2
+// INVOKE(f, t1, t2, ..., tN) is ((*t1).*f)(t2, ..., tN) 
+// when f is a pointer to a member function of a class T and
+// t1 is not one of the types described in the previous item;
+#include <memory>
+void test2()
+{
+  std::unique_ptr<Derived> p( new Derived(3) );
+  auto foo = &Base::foo;
+  auto bar = &Base::bar;
+  
+  BOOST_CHECK( etude::std_invoke( foo, p ) == 6 );
+  BOOST_CHECK( etude::std_invoke( bar, p, 2 ) == 10 );
+  {
+    etude::std_invoke<void>( foo, p );
+    BOOST_CHECK( p->member == 12 );
+    
+    auto x = etude::std_invoke<double>( bar, p, 0 );
+    STATIC_ASSERT(( std::is_same<double, decltype(x)>::value ));
+    BOOST_CHECK( x == 12 );
+  }
+}
+
+// test3
+// INVOKE(f, t1, t2, ..., tN) is t1.*f
+// when N == 1 and f is a pointer to member data of a class T and
+// t1 is an object of type T or a reference to an object of type T
+// or a reference to an object of a type derived from T;
+void test3()
+{
+  auto mem = &Base::member;
+  
+  // object of type T
+  BOOST_CHECK( etude::std_invoke( mem, Base() ) == 0 );
+  {
+    auto x = etude::std_invoke<double>( mem, Base(1) );
+    STATIC_ASSERT(( std::is_same<double, decltype(x)>::value ));
+    BOOST_CHECK( x == 1 );
+  }
+  
+  // reference to an object of type T
+  Base base(1);
+  BOOST_CHECK( etude::std_invoke( mem, base ) == 1 );
+  {
+    etude::std_invoke( mem, base ) = 2;
+    
+    auto x = etude::std_invoke<double>( mem, base );
+    STATIC_ASSERT(( std::is_same<double, decltype(x)>::value ));
+    BOOST_CHECK( x == 2 );
+  }
+  
+  // a reference to an object of a type derived from T
+  Derived derived(2);
+  BOOST_CHECK( etude::std_invoke( mem, derived ) == 2 );
+  {
+    etude::std_invoke( mem, derived ) *= 2;
+    
+    auto x = etude::std_invoke<double>( mem, derived );
+    STATIC_ASSERT(( std::is_same<double, decltype(x)>::value ));
+    BOOST_CHECK( x == 4 );
+  }
+}
+
+// test4
+// INVOKE(f, t1, t2, ..., tN) is (*t1).*f
+// when N == 1 and f is a pointer to member data of a class T and
+// t1 is not one of the types described in the previous item;
+void test4()
+{
+  std::unique_ptr<Derived> p( new Derived(3) );
+  auto mem = &Base::member;
+  
+  BOOST_CHECK( etude::std_invoke( mem, p ) == 3 );
+  {
+    auto x = etude::std_invoke<double>( mem, p );
+    STATIC_ASSERT(( std::is_same<double, decltype(x)>::value ));
+    BOOST_CHECK( x == 3 );
+  }
+}
+
+// f(t1, t2, ..., tN) in all other cases.
+int f(){ return 42; }
+void test5()
+{
+  auto x = etude::std_invoke( f );
+  STATIC_ASSERT(( std::is_same<int, decltype(x)>::value ));
+  BOOST_CHECK( x == 42 );
+  
+  auto x_ = etude::std_invoke( f );
+  STATIC_ASSERT(( std::is_same<decltype(x_), decltype(x)>::value ));
+  BOOST_CHECK( x == x_ );
+  
+  auto lambda = [&]( int i ){ return x += i; };
+  
+  auto y = etude::std_invoke<bool>( lambda, 13 );
+  STATIC_ASSERT(( std::is_same<bool, decltype(y)>::value ));
+  BOOST_CHECK( x == 55 && y == true );
+  
+  auto y_ = etude::std_invoke<bool>( lambda, 0 );
+  STATIC_ASSERT(( std::is_same<bool, decltype(y_)>::value ));
+  BOOST_CHECK( x == 55 && y_ == true );
+}
+
+// 型推論
+bool g( void const volatile* p ){ return p == 0; }
+
+void test6()
+{
+  struct X
+  {
+    void const volatile* p;
+    explicit X( void const volatile* p_ ) : p(p_) {}
+    bool foo( void const volatile* p_ ) const { return p == p_; }
+    
+  };
+  auto pf = &X::foo;
+  
+  X x(0), y( &x );
+  
+  auto b = etude::std_invoke( g, 0 );
+  STATIC_ASSERT(( std::is_same<bool, decltype(b)>::value ));
+  BOOST_CHECK( b );
+  
+  BOOST_CHECK( etude::std_invoke( pf, &x, 0 ) );
+}
+
+int test_main( int, char** )
+{
+  test1();
+  test2();
+  test3();
+  test4();
+  test5();
+  test6();
+  
+  return 0;
+}
