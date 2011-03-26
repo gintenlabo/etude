5207424c03cbd4b61fb1fb5528e90918eef3c7f1
SubaruG <gintensubaru@gmail.com>
2011-03-26 10:53:24 +0900

    is_xxx_comparable の定義を変更

--- etude/types/is_equality_comparable.hpp
+++ etude/types/is_equality_comparable.hpp
@@ -14,39 +14,18 @@
 #ifndef ETUDE_TYPES_INCLUDED_IS_EQUALITY_COMPARABLE_HPP_
 #define ETUDE_TYPES_INCLUDED_IS_EQUALITY_COMPARABLE_HPP_
 
-#include <type_traits>
-#include <utility>
-#include "../utility/compare_equal_to.hpp"
+#include "is_simply_callable.hpp"
+#include "../functional/equal_to.hpp"
 
 namespace etude {
 
-  // 実装用
-  template<class T, class U, class = void>
-  class is_equality_comparable_
-  {
-    template< class T_ = T, class U_ = U,
-      class R = decltype(
-        etude::compare_equal_to( std::declval<T_>(), std::declval<U_>() )
-      ),
-      class = typename std::enable_if< std::is_convertible<R, bool>::value >::type
-    >
-    static std::true_type test_( int );
-    
-    static std::false_type test_( ... );
-    
-   public:
-    typedef decltype( test_(0) ) type;
-    
-  };
-
-  // 本体
   template<class T, class U = T>
   struct is_equality_comparable :
-    is_equality_comparable_<
-      typename std::conditional<std::is_reference<T>::value, T, T const&>::type,
-      typename std::conditional<std::is_reference<U>::value, U, U const&>::type
+    etude::is_simply_callable<
+      etude::equal_to<T, U>, bool ( T&&, U&& )
     >::type
-  {};
+  {
+  };
 
 } // namespace etude
 
--- etude/types/is_less_than_comparable.hpp
+++ etude/types/is_less_than_comparable.hpp
@@ -16,39 +16,18 @@
 #ifndef ETUDE_TYPES_INCLUDED_IS_LESS_THAN_COMPARABLE_HPP_
 #define ETUDE_TYPES_INCLUDED_IS_LESS_THAN_COMPARABLE_HPP_
 
-#include <type_traits>
-#include <utility>
-#include "../utility/compare_less.hpp"
+#include "is_simply_callable.hpp"
+#include "../functional/less.hpp"
 
 namespace etude {
 
-  // 実装用
-  template<class T, class U, class = void>
-  class is_less_than_comparable_
-  {
-    template< class T_ = T, class U_ = U,
-      class R = decltype(
-        etude::compare_less( std::declval<T_>(), std::declval<U_>() )
-      ),
-      class = typename std::enable_if< std::is_convertible<R, bool>::value >::type
-    >
-    static std::true_type test_( int );
-    
-    static std::false_type test_( ... );
-    
-   public:
-    typedef decltype( test_(0) ) type;
-    
-  };
-
-  // 本体
   template<class T, class U = T>
   struct is_less_than_comparable :
-    is_less_than_comparable_<
-      typename std::conditional<std::is_reference<T>::value, T, T const&>::type,
-      typename std::conditional<std::is_reference<U>::value, U, U const&>::type
+    etude::is_simply_callable<
+      etude::less<T, U>, bool ( T&&, U&& )
     >::type
-  {};
+  {
+  };
 
 } // namespace etude
 
