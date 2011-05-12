dff0f7a769d79585adc47693868dad9982bc681f
SubaruG <gintensubaru@gmail.com>
2011-05-12 17:32:45 +0900

    unrebindable を追加

---
+++ etude/unrebindable.hpp
@@ -0,0 +1,83 @@
+//
+//  unrebindable:
+//    再代入できないクラス
+// 
+//    再代入を delete したクラスです．
+//    また、テンプレート引数により base class chaining を行うこともできます。
+//    
+//  Copyright (C) 2011  Takaya Saito (SubaruG)
+//    Distributed under the Boost Software License, Version 1.0.
+//    http://www.boost.org/LICENSE_1_0.txt
+//
+#ifndef ETUDE_INCLUDED_UNREBINDABLE_HPP_
+#define ETUDE_INCLUDED_UNREBINDABLE_HPP_
+
+#include <type_traits>
+#include <utility>
+
+#include "types/is_constructible.hpp"
+#include "types/make_derivable.hpp"
+
+namespace etude {
+ namespace unrebindable_ { // ADL 回避
+ 
+  template<class Base = void>
+  class unrebindable
+    : public etude::make_derivable<Base>::type
+  {
+    typedef typename etude::make_derivable<Base>::type base_;
+    
+   public:
+    unrebindable() = default;
+    
+    // gcc-4.5.x には implicit move がないので = default; する
+    unrebindable( unrebindable const& ) = default;
+    unrebindable( unrebindable && )     = default;
+    
+    // 再代入禁止
+    void operator=( unrebindable const& ) = delete;
+    // なくてもよい
+    void operator=( unrebindable&& )      = delete;
+    
+    
+    // Base の構築
+    
+    // Base 自体から構築。ただし explicit
+    template< class T = Base,
+      class = typename std::enable_if<
+        etude::is_constructible<Base, T&&>::value
+      >::type
+    >
+    explicit unrebindable( Base && x )
+      : base_( std::forward<Base>(x) ) {}
+    
+    // その他の構築も全て explicit
+    template< class... Args,
+      class = typename std::enable_if<
+        etude::is_constructible<Base, Args...>::value
+      >::type
+    >
+    explicit unrebindable( Args&&... args )
+      : base_( std::forward<Args>(args)... ) {}
+    
+  };
+ 
+  template<>
+  struct unrebindable<void>
+  {
+    unrebindable() = default;
+    
+    // 空なら move は関係ないので = default; しない
+    
+    // 再代入禁止
+    void operator=( unrebindable const& ) = delete;
+    // なくてもよい
+    void operator=( unrebindable&& )      = delete;
+    
+  };
+ 
+ }
+ using namespace unrebindable_;
+}
+
+#endif  // #ifndef ETUDE_INCLUDED_UNREBINDABLE_HPP_
---
+++ tests/unrebindable.cc
@@ -0,0 +1,61 @@
+//
+//  etude/unrebindable.hpp に対するテスト
+//    というより仕様
+//
+//  Copyright (C) 2011  Takaya Saito (SubaruG)
+//    Distributed under the Boost Software License, Version 1.0.
+//    http://www.boost.org/LICENSE_1_0.txt
+//
+#include "../etude/unrebindable.hpp"
+
+#include <type_traits>
+#include "../etude/types/is_constructible.hpp"
+#define STATIC_ASSERT( expr ) static_assert( expr, #expr )
+
+// for is_xxx_constructible/assignable
+#include "test_utilities.hpp"
+
+int main()
+{
+  // etude::unrebindable<> に対して
+  STATIC_ASSERT((  is_copy_constructible<etude::unrebindable<>>::value ));
+  STATIC_ASSERT((  is_move_constructible<etude::unrebindable<>>::value ));
+  
+  STATIC_ASSERT(( !is_copy_assignable<etude::unrebindable<>>::value ));
+  STATIC_ASSERT(( !is_move_assignable<etude::unrebindable<>>::value ));
+  
+  STATIC_ASSERT(( std::is_empty<etude::unrebindable<>>::value ));
+  
+  STATIC_ASSERT(( std::has_trivial_default_constructor<etude::unrebindable<>>::value ));
+  STATIC_ASSERT(( std::has_trivial_destructor<etude::unrebindable<>>::value ));
+
+
+  // base class chaining
+  struct X {};
+  
+  STATIC_ASSERT(( std::is_base_of<X, etude::unrebindable<X>>::value ));
+
+  STATIC_ASSERT((  is_copy_constructible<etude::unrebindable<X>>::value ));
+  STATIC_ASSERT((  is_move_constructible<etude::unrebindable<X>>::value ));
+  
+  STATIC_ASSERT(( !is_copy_assignable<etude::unrebindable<X>>::value ));
+  STATIC_ASSERT(( !is_move_assignable<etude::unrebindable<X>>::value ));
+  
+  // ctor 転送
+  STATIC_ASSERT(( !std::is_convertible<X&&, etude::unrebindable<X>>::value ));
+  STATIC_ASSERT(( etude::is_constructible<etude::unrebindable<X>, X&&>::value ));
+  STATIC_ASSERT(( !etude::is_constructible<etude::unrebindable<X>, int>::value ));
+
+
+  // unrebindable wrapper
+  STATIC_ASSERT((  is_copy_constructible<etude::unrebindable<int>>::value ));
+  STATIC_ASSERT((  is_move_constructible<etude::unrebindable<int>>::value ));
+  
+  STATIC_ASSERT(( !is_copy_assignable<etude::unrebindable<int>>::value ));
+  STATIC_ASSERT(( !is_move_assignable<etude::unrebindable<int>>::value ));
+  
+  // ctor 転送
+  STATIC_ASSERT(( !std::is_convertible<int, etude::unrebindable<int>>::value ));
+  STATIC_ASSERT(( etude::is_constructible<etude::unrebindable<int>, int>::value ));
+  STATIC_ASSERT(( !etude::is_constructible<etude::unrebindable<int>, void*>::value ));
+}
