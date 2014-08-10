64e0975bb65cfa7acae96463da8c598f23fcb1df
Takaya SAITO (SubaruG) <gintensubaru@gmail.com>
2014-08-10 16:33:51 +0900

    Fix etude::forward

--- etude/utility/forward.hpp
+++ etude/utility/forward.hpp
@@ -9,6 +9,7 @@
 #ifndef ETUDE_UTILITY_INCLUDED_FORWARD_HPP_
 #define ETUDE_UTILITY_INCLUDED_FORWARD_HPP_
 
+#include <type_traits>
 #include "avoid_deduction.hpp"
 
 namespace etude {
@@ -22,12 +23,14 @@ constexpr T&& move(T& x) noexcept {
 }
 
 // constexpr version of std::forward
-// unlike std::forward, rvalue reference version is not defined
-// (you should not use this function like `etude::forward<T>(f())`)
+// unlike std::forward, rvalue reference version is deleted for all type
+// (you should use this function iff you are forwarding lvalues)
 template<class T>
 constexpr T&& forward(typename avoid_deduction<T&>::type x) noexcept {
   return static_cast<T&&>(x);
 }
+template<class T>
+void forward(typename std::remove_reference<T>::type&& x) = delete;
 
 // simple macro for generic lambdas
 // var should be id-expression
